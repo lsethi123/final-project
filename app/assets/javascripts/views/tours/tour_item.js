7203f3c078c8abc98381c1d303af2c76a1b8054e
Lisa Cho <lmidoricho@gmail.com>
2015-08-16 14:42:18 -0700

    Fix thumbnail on tour show pg

--- app/assets/javascripts/views/tours/tour_item.js
+++ app/assets/javascripts/views/tours/tour_item.js
@@ -9,10 +9,9 @@ Tryable.Views.TourItem = Backbone.View.extend({
 
   render: function (){
     var content = this.template( {tour: this.model });
-    var images = this.model.images();
-    var img_first = _.first(images);
     var img;
-    if (images.count > 0 ){
+    if (this.model.images() ){
+      img_first = this.model.images().models[0];
       img = $.cloudinary.image(img_first.escape('url'), {width: 200, height: 200, crop: 'fill' });
       img.addClass('tour-img');
     }
