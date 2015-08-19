d4f9e6c42558bee8f4a79c6c26a010beb2bd33e3
Lisa Cho <lmidoricho@gmail.com>
2015-08-18 17:49:45 -0700

    Add stop scrolling and correct position for modal

--- app/assets/javascripts/views/images/modal.js
+++ app/assets/javascripts/views/images/modal.js
@@ -2,28 +2,45 @@ Tryable.Views.ImageModal = Backbone.View.extend({
   template: JST['images/slide'],
 
   events: {
-    'click .m-background': 'remove',
+    'click .m-background': 'removeModal',
     'click .close': 'removeBtn'
   },
 
   initialize: function (options) {
     this.first = options.first
     $(document).on('keyup', this.handleKey.bind(this));
+    $('body').addClass('stop-scrolling');
   },
 
   handleKey: function (event) {
     if (event.keyCode === 27) {
+      $('body').removeClass('stop-scrolling');
       this.remove();
     }
   },
 
+  removeModal: function(){
+    $('body').removeClass('stop-scrolling');
+    this.remove();
+  },
+
   removeBtn: function (event) {
     event.preventDefault();
+    $('body').removeClass('stop-scrolling');
     this.remove();
   },
 
   render: function () {
     this.$el.html(this.template());
+    this.onRender();
+    var y = window.pageYOffset;
+    console.log(y);
+    this.$('.m-background').css('top', y+'px');
+    this.$('.m-content').css('top', y+40+'px');
+    return this;
+  },
+
+  onRender: function(){
     this.collection.each( function (image) {
       var newSlide = $('<div></div>').addClass('item');
       if (this.model.get('url') === image.get('url')) {
@@ -31,9 +48,8 @@ Tryable.Views.ImageModal = Backbone.View.extend({
       }
       var image = $.cloudinary.image(image.escape('url'), {width: 1000, height: 600, crop: 'fill'});
       newSlide.append(image);
-      $('.carousel-inner').append(newSlide);
+      this.$('.carousel-inner').append(newSlide);
     }.bind(this));
-    return this;
-  },
+  }
 
 });
